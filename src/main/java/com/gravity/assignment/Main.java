e893915c417cf03db583db51243c93c86c025155
ertanngul <ertann.gul@gmail.com>
2018-08-05 22:45:13 +0300

    Update Main.java
    final version

--- src/main/java/com/gravity/assignment/Main.java
+++ src/main/java/com/gravity/assignment/Main.java
@@ -2,15 +2,15 @@ package com.gravity.assignment;
 
 import com.gravity.assignment.entities.DistanceCalculationResult;
 import com.gravity.assignment.entities.Point;
-import com.gravity.assignment.exceptions.MalStructuredInputFileException;
 import com.gravity.assignment.exceptions.MissingInputFileException;
 import com.gravity.assignment.services.CalculationService;
 import com.gravity.assignment.services.PointService;
 import com.gravity.assignment.utils.FileOperationsUtil;
-import java.io.IOException;
 import java.util.List;
 
 public class Main {
+
+  private static int ALGORITHM_THRESHOLD = 10;
 
   public static void main(String[] args) {
     FileOperationsUtil fileOperationsUtil = new FileOperationsUtil();
@@ -21,16 +21,20 @@ public class Main {
       if (args.length == 0) {
         throw new MissingInputFileException(
             "Input file is not provided. Please provide full path of input file as an argument like:\n mvn exec:java "
-                + "-Dexec.mainClass=\"com.gravity.assignment.Main\" -Dexec.args=\"full/path/of/input/file\"");
+                + "-Dexec.mainClass=\"com.gravity.assignment.Main\" -Dexec.args=\"full_path_of_the_input_file\"");
       }
 
-      List<Point> pointList = fileOperationsUtil.parseFile(args[0]);
+      String inputFilePath = args[0];
+      List<Point> pointList = fileOperationsUtil.parseFile(inputFilePath);
       PointService.setDimensionCount(pointList.get(0).getCoordinates().size());
       DistanceCalculationResult d = null;
 
-      if (PointService.getDimensionCount() < 20) {
-        d = calculationService.shortestDistanceFoundBrutally(pointList);
+
+      if (PointService.getDimensionCount() < ALGORITHM_THRESHOLD) {
+        System.out.println("Closest points algorithm is utilizing...");
+        d = calculationService.calculateShortestDistance(pointList);
       } else {
+        System.out.println("Brute-force algorithm is utilizing...");
         d = calculationService.shortestDistanceFoundBrutally(pointList);
       }
 
@@ -38,10 +42,11 @@ public class Main {
       System.out.println("#");
       System.out.println("#");
       System.out.println(d.toString());
+      fileOperationsUtil.writeResultToFile(inputFilePath, d);
       System.out.println("#");
       System.out.println("#");
       System.out.println("###############################################################");
-    } catch (IOException | MissingInputFileException | MalStructuredInputFileException e) {
+    } catch (Exception e) {
       System.err.println("#########################EXCEPTION#############################");
       System.err.println(e.getMessage());
       System.err.println("###############################################################");
