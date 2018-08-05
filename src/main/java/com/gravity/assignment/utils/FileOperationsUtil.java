20f076236773cc5244fbbf79a750fcf21cc47c9c
ertanngul <ertann.gul@gmail.com>
2018-08-05 22:45:53 +0300

    Update FileOperationsUtil.java
    final version

--- src/main/java/com/gravity/assignment/utils/FileOperationsUtil.java
+++ src/main/java/com/gravity/assignment/utils/FileOperationsUtil.java
@@ -1,31 +1,38 @@
 package com.gravity.assignment.utils;
 
+import com.gravity.assignment.entities.DistanceCalculationResult;
 import com.gravity.assignment.entities.Point;
 import com.gravity.assignment.exceptions.MalStructuredInputFileException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 
 public class FileOperationsUtil {
 
-  public List<Point> parseFile(String fileName) throws IOException, MalStructuredInputFileException {
-    List<Point> pointList = new ArrayList<>();
+  public List<Point> parseFile(String fileName)
+      throws IOException, MalStructuredInputFileException {
+    List<Point> pointList = new ArrayList();
 
     BufferedReader br = new BufferedReader(new FileReader(fileName));
     String sCurrentLine;
     int index = 0;
 
     while ((sCurrentLine = br.readLine()) != null) {
-      List<Double> coordinateList = Arrays.stream(sCurrentLine.split("\\s+")).map(String::trim)
-          .mapToDouble(Double::parseDouble).boxed().collect(Collectors.toList());
+      List<Double> coordinateList = new ArrayList<Double>();
+      String[] coordinates = sCurrentLine.split("\\s+");
+      for(int i=0;i<coordinates.length;i++){
+        coordinateList.add(Double.parseDouble(coordinates[i]));
+      }
 
-      if(CollectionUtils.isNotEmpty(pointList) && pointList.get(pointList.size()-1).getCoordinates().size() != coordinateList.size()){
-        throw new MalStructuredInputFileException("There is an inconsistency between the dimension of points in the input file. Please restructure your input file. ");
+      if (CollectionUtils.isNotEmpty(pointList)
+          && pointList.get(pointList.size() - 1).getCoordinates().size() != coordinateList.size()) {
+        throw new MalStructuredInputFileException(
+            "There is an inconsistency between the dimension of points in the input file. Please restructure your input file. ");
       }
 
       Point point = new Point();
@@ -35,5 +42,19 @@ public class FileOperationsUtil {
     }
 
     return pointList;
+  }
+
+  public void writeResultToFile(String inputFileName,
+      DistanceCalculationResult distanceCalculationResult) throws IOException {
+    String[] inputFileNameArray = inputFileName.split("/");
+    String outputFileName = StringUtils.EMPTY;
+    for(int i=0;i<inputFileNameArray.length-1;i++){
+      outputFileName += inputFileNameArray[i] + "/";
+    }
+    outputFileName += "output_" + inputFileNameArray[inputFileNameArray.length-1];
+
+    PrintWriter writer = new PrintWriter(outputFileName, "UTF-8");
+    writer.println(distanceCalculationResult.toString());
+    writer.close();
   }
 }
